23f88e9033c69538b27acc5f7daa142af8877ae7
Pierre Maris <me@pierremaris.com>
2018-10-22 19:26:51 -0700

    Add unit tests for geotag finder

--- js/geotag_finder.js
+++ js/geotag_finder.js
@@ -4,131 +4,85 @@ var recursive = require('recursive-readdir');
 var exifParser = require('exif-parser');
 
 module.exports = {
-    getGeotaggedPhotos: getGeotaggedPhotos,
-    getNonGeotaggedPhotos: getNonGeotaggedPhotos,
-    getPhotosMatchingFilters: getPhotosMatchingFilters,
-    getSanitizedExtensions: getSanitizedExtensions,
-    ignoreFile: ignoreFile,
-    getImageExif: getImageExif
-}
-
-/**
- * Get the details of photos in a given array of file paths that are geotagged. Specifically, photos
- * that contain valid GPSLatitude and GPSLongitude tags in their EXIF metadata are considered to be
- * geotagged.
- * @param (string[]) photoPaths Absolute paths of photos to search through for geotagged photos.
- * @return {object[]} Details of all photos from within the directory tree that are geotagged, with
- *                    each object containing the following keys:
- *                        filePath: Absolute path of the image file.
- *                        latitude: The latitude of the location where the photo was taken.
- *                        longitude: The longitude of the location where the photo was taken.
- *                        createTime: Unix timestamp representing the time the photo was taken, or 0
- *                            if the photo does not contain the DateTimeOriginal tag.
- */
-function getGeotaggedPhotos(photoPaths) {
-        try {
-            exif = getImageExif(photoPaths[i]);
-        }
-        catch (err) {
-            return null;
-        }
-
-        if (exif.tags.GPSLatitude && exif.tags.GPSLongitude) {
-            metadata = {
-                'filePath': photoPaths[i],
-                'latitude': exif.tags.GPSLatitude,
-                'longitude': exif.tags.GPSLongitude,
-                'createTime': 'DateTimeOriginal' in exif.tags ? exif.tags.DateTimeOriginal : 0
-            }
-        }
-
-    return metadata;
-}
-
-/**
- * Get the paths of photos in a given array of file paths that are not geotagged. Specfically,
- * photos without valid GPSLatitude and GPSLongitude tags in their EXIF metadata are considered to
- * not be geotagged. This includes images that contain no EXIF metadata at all, or where the EXIF
- * could not be read.
- * @param (string[]) photoPaths Absolute paths of photos to search through for geotagged photos.
- * @param (function) counterHandler
- * @return {string[]} Absolute paths of all photos from within the directory tree that do not have a
- *                    location in their EXIF metadata.
- */
-function getNonGeotaggedPhotos(photoPaths, counterHandler) {
-    var nonGeotaggedPhotos = [];
-    var exif;
-
-    for (i = 0; i < photoPaths.length; i++) {
-        console.log('Checking whether file %s is geotagged', photoPaths[i]);
-        try {
-            exif = getImageExif(photoPaths[i]);
-            if (!exif.tags.GPSLatitude || !exif.tags.GPSLongitude) {
-                notGeotaggedPhotos.push(photoPaths[i]);
-            }
-        }
-        catch (err) {
-            console.warn('Could not read EXIF metadata for due to error', err);
-            notGeotaggedPhotos.push(photoPaths[i]);
-        }
-    }
-    return nonGeotaggedPhotos;
+  getPhotoPaths: getPhotoPaths,
+  getSanitizedExtensions: getSanitizedExtensions,
+  getPhotoExif: getPhotoExif,
+  ignoreFile: ignoreFile
 }
 
 /**
  * Retrieves the EXIF metadata of an image file.
- * @param {string} imagePath Absolute path of an image file.
- * @return {object} Contents of the image's EXIF metadata tags.
+ * @param {string} photoPath Absolute path of an image file.
+ * @return {object} Contents of the image's EXIF metadata tags. If the file
+ *                  cannot be read or does not contain EXIF metadata, null will
+ *                  will be returned.
  */
-function getImageExif(imagePath) {
-    console.log(imagePath);
-    // EXIF metadata will always occur in the first 64KB of an image file, so only that much of the
-    // file contents needs to be read
-    var fd = fs.openSync(imagePath, 'r');
-    var buffer = new Buffer(65535);
+function getPhotoExif (photoPath) {
+  console.log(photoPath);
+  try {
+    var fd = fs.openSync(photoPath, 'r');
+    // EXIF metadata will always occur in the first 64KB of an image file, so
+    // only that much of the file contents needs to be read
+    var buffer = Buffer.alloc(65535);
     fs.readSync(fd, buffer, 0, buffer.length, 0);
     fs.closeSync(fd);
     return exifParser.create(buffer).parse();
-}
-
-function getPhotosMatchingFilters(baseDirectory, fileExtensions, callback) {
-    var sanitizedExtensions = getSanitizedExtensions(fileExtensions);
-    recursive(baseDirectory, [function(file, stats) {
-        return ignoreFile(file, stats, sanitizedExtensions);
-    }], function (err, files) {
-        if (err) {
-            console.error('An error ocurred reading when the directory: ' + err);
-            return;
-        }
-        callback(files);
-    });
-
+  } catch (err) {
+    console.error('Could not open file due to error', err);
+    return null;
+  }
 }
 
 /**
- * Sanitizes an array of file extensions by converting all of them to lowercase and removing
- * periods.
- * @param {string[]} fileExtensions The file extensions to santize.
- * @return {string[]} Sanitized array of file extensions.
+ * Get the absolute paths of all photos in a directory tree with specified file
+ * extensions.
+ * @param {string} baseDirectory The base directory in a directory tree to
+ *                               search for photos.
+ * @param {string[]} fileExtensions Case-insensitive file extensions of the
+ *                                  files to search for.
+ * @param {function} callback Function to call with the paths of photos that
+ *                            were found. This function will be passed a single
+ *                            argument, an array of unsorted absolute paths of
+ *                            the photos that were found.
  */
-function getSanitizedExtensions(fileExtensions) {
-     var sanitizedExtensions = fileExtensions.map(function(currentValue) {
-        return currentValue.toLowerCase().replace('.', '');
-    });
-    return sanitizedExtensions;
+function getPhotoPaths (baseDirectory, fileExtensions, callback) {
+  var sanitizedExtensions = getSanitizedExtensions(fileExtensions);
+  return recursive(baseDirectory, [function (file, stats) {
+    return ignoreFile(file, stats, sanitizedExtensions);
+  }], function (err, files) {
+    if (err) {
+      console.error('An error ocurred when reading the directory: ' + err);
+      callback([]);
+    } else {
+      callback(files);
+    }
+  });
 }
 
-/** Determines if a file should be excluded from an array of files returned by the recursize-readdir
- * package.
+/**
+ * Sanitizes an array of file extensions by converting all of them to lowercase
+ * and removing periods.
+ * @param {string[]} fileExtensions The file extensions to sanitize.
+ * @return {string[]} Sanitized array of file extensions.
+ */
+function getSanitizedExtensions (fileExtensions) {
+  var sanitizedExtensions = fileExtensions.map(function (currentValue) {
+    return String(currentValue).toLowerCase().replace('.', '');
+  });
+  return sanitizedExtensions;
+}
+
+/** Determines if a file should be excluded from an array of files returned by
+ * the recursize-readdir package.
  * @param {string} file Absolute path of a file.
  * @param {object} stats Stats object returned from fs.lstat()
- * @param {string[]} sanitizedExtensions File extensions to be ignored, sanitized to be entirely
- *                                       lowercase and not include periods.
- * @return {boolean} Indicates if the boolean should be ignore; true if the file should be ignored.
+ * @param {string[]} sanitizedExtensions File extensions to be ignored,
+                                         sanitized to be entirely lowercase and
+ *                                       not include periods.
+ * @return {boolean} Indicates whether the file should be ignored, true if the
+ *                   file should be ignored.
  */
-function ignoreFile(file, stats, sanitizedExtensions) {
-    var extension = path.extname(file).toLowerCase().replace('.', '');
-    // Test whether the file is a directory and whether the file extension is in the list of allowed
-    // extensions
-    return !stats.isDirectory() && sanitizedExtensions.indexOf(extension) == -1;
+function ignoreFile (file, stats, sanitizedExtensions) {
+  var extension = path.extname(file).toLowerCase().replace('.', '');
+  return !stats.isDirectory() && sanitizedExtensions.indexOf(extension) === -1;
 }
--- test/test_geotag_finder.js
+++ test/test_geotag_finder.js
@@ -1,29 +1,94 @@
 var assert = require('assert');
+var path = require('path');
+var sinon = require('sinon');
 var geotagFinder = require('../js/geotag_finder.js');
 
-describe('geotag_finder', function() {
-
-    describe('#getSanitizedExtensions()', function() {
-        it('should return an array of extensions converted to lowercase', function() {
-            assert.deepEqual(['jpg', 'cr2'], geotagFinder.getSanitizedExtensions(['JPG', 'CR2']));
-        });
-
-        it('should remove periods from extensions', function() {
-            assert.deepEqual(['jpg', 'cr2'], geotagFinder.getSanitizedExtensions(['.jpg', '.cr2']));
-        });
+describe('geotag_finder', function () {
+  describe('#getPhotoExif()', function () {
+    it('should return null if the file cannot be read', function () {
+      assert.strictEqual(geotagFinder.getPhotoExif('abcd'), null);
     });
 
-    describe('#ignoreFile()', function() {
-        it('should return false when the file path is a directory', function() {
-            assert.equal(false, geotagFinder.ignoreFile('abcd.jpg', {isDirectory: function(){return true}}, ['jpg']));
-        });
-
-        it('should return false when the file path is not a directory and the file extension is in the array of sanitized extensions', function() {
-            assert.equal(false, geotagFinder.ignoreFile('abcd.jpg', {isDirectory: function(){return false}}, ['jpg']));
-        });
-
-        it('should return true when the file path is not a directory and the file extension is not in the array of sanitized extensions', function() {
-            assert.equal(true, geotagFinder.ignoreFile('abcd.cr2', {isDirectory: function(){return false}}, ['jpg']));
-        });
+    it('should return null if the file path is a directory', function () {
+      assert.strictEqual(geotagFinder.getPhotoExif(__dirname), null);
     });
+
+    it('should return null if the file does not contain EXIF metadata', function () {
+      assert.strictEqual(geotagFinder.getPhotoExif(path.resolve(__dirname, 'data', 'file_without_exif')), null);
+    });
+
+    it('should return the EXIF metadata for an image', function () {
+      var exif = geotagFinder.getPhotoExif(path.resolve(__dirname, 'data', 'image_with_exif.jpg'));
+      assert.notStrictEqual(exif, null);
+
+      // Test a subset of the fields in the returned EXIF data, specifically the
+      // fields that are used by the application
+      assert.strictEqual(exif.tags.DateTimeOriginal, 1503423638);
+      assert.strictEqual(exif.tags.GPSLatitude, 46.585755679985255);
+      assert.strictEqual(exif.tags.GPSLongitude, -112.01842411999628);
+    });
+  });
+
+  describe('#getPhotoPaths()', function () {
+    it('should return the paths to all photos in the directory that have the specified extensions', function (done) {
+      const photosDir = path.resolve(__dirname, 'data', 'photos');
+      geotagFinder.getPhotoPaths(photosDir, ['jpg'], function (files) {
+        assert.deepStrictEqual(files.sort(), [path.resolve(photosDir, 'a', 'c', 'c.jpg'), path.resolve(photosDir, 'b', 'b.jpg')]);
+        done();
+      });
+    })
+
+    it('should return an empty array if no files in the directory have the specified extensions', function (done) {
+      geotagFinder.getPhotoPaths(path.resolve(__dirname, 'data', 'photos'), ['tiff'], function (files) {
+        assert.deepStrictEqual(files, []);
+        done();
+      });
+    });
+
+    it('should return an empty array if given a path to directory that doesnt exist', function (done) {
+      geotagFinder.getPhotoPaths('/foo/bar', ['jpg'], function (files) {
+        assert.deepStrictEqual(files, []);
+        done();
+      });
+    });
+
+    it('should return an empty array if given a path to a file', function (done) {
+      geotagFinder.getPhotoPaths(__filename, ['jpg'], function (files) {
+        assert.deepStrictEqual(files, []);
+        done()
+      });
+    })
+  });
+
+  describe('#getSanitizedExtensions()', function () {
+    it('should return an array of extensions converted to lowercase', function () {
+      assert.deepStrictEqual(geotagFinder.getSanitizedExtensions(['JPG', 'CR2']), ['jpg', 'cr2']);
+    });
+
+    it('should remove periods from extensions', function () {
+      assert.deepStrictEqual(geotagFinder.getSanitizedExtensions(['.jpg', '.CR2']), ['jpg', 'cr2']);
+    });
+
+    it('should return an empty array if given an empty array', function () {
+      assert.deepStrictEqual(geotagFinder.getSanitizedExtensions([]), []);
+    });
+
+    it('should convert all values in the array to strings', function () {
+      assert.deepStrictEqual(geotagFinder.getSanitizedExtensions([123, true, null]), ['123', 'true', 'null']);
+    });
+  });
+
+  describe('#ignoreFile()', function () {
+    it('should return false when the path is a directory', function () {
+      assert.strictEqual(geotagFinder.ignoreFile('abcd.jpg', { isDirectory: function () { return true } }, ['jpg']), false);
+    });
+
+    it('should return false when the file extension is in the array of sanitized extensions', function () {
+      assert.strictEqual(geotagFinder.ignoreFile('abcd.jpg', { isDirectory: function () { return false } }, ['jpg']), false);
+    });
+
+    it('should return true when the the file extension is not in the array of sanitized extensions', function () {
+      assert.strictEqual(geotagFinder.ignoreFile('abcd.cr2', { isDirectory: function () { return false } }, ['jpg']), true);
+    });
+  });
 });
