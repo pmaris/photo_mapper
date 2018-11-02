de398fb4a831d7d9274b86740081f8dfeba3009b
Pierre Maris <me@pierremaris.com>
2018-11-01 23:57:04 -0700

    Fix export for Photo model

--- js/model.js
+++ js/model.js
@@ -1,16 +1,16 @@
 const Sequelize = require('sequelize');
 
-module.exports = {
-    Photo: Photo,
-};
-
 const sequelize = new Sequelize('sqlite:../geotags.db');
 
 const Photo = sequelize.define('photo', {
-    path: Sequelize.STRING,
-    latitude: Sequelize.DOUBLE,
-    longitude: Sequelize.DOUBLE,
-    create_time: Sequelize.INTEGER
+  path: Sequelize.STRING,
+  latitude: Sequelize.DOUBLE,
+  longitude: Sequelize.DOUBLE,
+  create_time: Sequelize.INTEGER
 });
 
 Photo.sync();
+
+module.exports = {
+  Photo: Photo
+};
