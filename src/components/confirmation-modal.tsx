12b9613320801a259557595bafcc54293c0e5569
Pierre Maris <pierre.a.maris@gmail.com>
2025-03-01 16:28:33 -0800

    Create progress modal

--- src/components/confirmation-modal.tsx
+++ src/components/confirmation-modal.tsx
@@ -1,11 +1,23 @@
-import React from "react"
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 
-export default function ConfirmationModal({ title, message }) {
+export default function ConfirmationModal({ title, message, show, onClose, onConfirm }: { title: string, message: string, show: boolean, onClose: () => void, onConfirm: () => void }) {
     return (
-        <div id="confirmation-modal" title={title} hidden>
-            <p>
-                {message}
-            </p>
+        <div id="confirmation-modal">
+            <Modal show={show} onHide={onClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>{title}</Modal.Title>
+                </Modal.Header>
+
+                <Modal.Body>
+                    <p>{message}</p>
+                </Modal.Body>
+
+                <Modal.Footer>
+                <Button variant="secondary" onClick={onClose}>Close</Button>
+                <Button variant="primary" onClick={onConfirm}>Confirm</Button>
+                </Modal.Footer>
+            </Modal>
         </div>
     )
 }
--- src/components/progress-modal.tsx
+++ src/components/progress-modal.tsx
@@ -1,13 +1,56 @@
-import React from "react"
+import Button from 'react-bootstrap/Button';
+import ProgressBar from 'react-bootstrap/ProgressBar';
+import Modal from 'react-bootstrap/Modal';
+import React from 'react';
+import ConfirmationModal from './confirmation-modal';
+import '../../css/photo_mapper.css';
 
-export default function ProgressModal() {
+export default function ProgressModal({count, maxValue, show, setShow, setAbort}: { count: number, maxValue: number, show: boolean, setShow: (value: boolean) => void, setAbort: (value: boolean) => void }) {
+    const [complete, setComplete] = React.useState(false);
+    const [progressLabel, setProgressLabel] = React.useState("Counting photos...");
+    const [showCancelModal, setShowCancelModal] = React.useState(false)
+
+    function cancel() {
+        setAbort(true);
+        setShowCancelModal(false);
+        setShow(false);
+    }
+
+    React.useEffect(() => {
+        if (maxValue && count == maxValue) {
+            setComplete(true);
+            setProgressLabel('Finished');
+        }
+        else if (maxValue) {
+            setProgressLabel(`${count} / ${maxValue} photos checked`);
+        }
+        else {
+            setProgressLabel("Counting photos...");
+        }
+    }, [count, maxValue])
+
     return (
-        <div id="progress-modal" hidden>
-        <div id="finder-progress-bar">
-            <div id="progress-bar-label">Counting photos...</div>
-        </div>
-        <p id="finder-result" hidden></p>
-            <button id="finderResultCloseButton" hidden>Close</button>
-        </div>
+        <Modal show={show}>
+            <Modal.Header>
+                <Modal.Title>Searching for geotagged photos</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <ProgressBar min={0} now={count} max={maxValue} label={progressLabel}/>
+            </Modal.Body>
+            <Modal.Footer>
+                { complete ? (
+                    <Button variant="secondary" onClick={() => { setShow(false) }}>Close</Button>
+                ) : (
+                    <Button variant="danger" onClick={() => { setShowCancelModal(true) }}>Cancel</Button>
+                )}
+            </Modal.Footer>
+            <ConfirmationModal 
+                title="Cancel search"
+                message="Cancel the search for geotagged photos? Photos that have been found will not be saved." 
+                show={showCancelModal}
+                onClose={() => setShowCancelModal(false)}
+                onConfirm={cancel}
+            />
+        </Modal>
     )
 }
